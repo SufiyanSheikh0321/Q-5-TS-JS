347d72b623b2ceba47642a47cef901c383337f60
Muhammed Sufiyan <148213204+SufiyanSheikh0321@users.noreply.github.com>
2024-02-29 19:04:25 +0500

    Add files via upload

---
+++ Q.5.js
@@ -0,0 +1,5 @@
+"use strict";
+// 5. Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous person. Then compose your message and store it in a new variable called message. print your message. 
+let famous_person = "Hazarat Ali";
+let message = "Accept The Apology, Even If It Is Not Sincer";
+console.log(`${famous_person} Once Said, "${message}"`);
---
+++ Q.5.ts
@@ -0,0 +1,8 @@
+// 5. Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous person. Then compose your message and store it in a new variable called message. print your message. 
+
+let famous_person: string = "Hazarat Ali"
+let message: string = "Accept The Apology, Even If It Is Not Sincer"
+
+ console.log(`${famous_person} Once Said, "${message}"`)
+
+
